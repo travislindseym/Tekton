b211debdfeb0cce639690e0c2b6b7bb9ad9a61ab
Travis Lindsey <travislindseym@gmail.com>
2022-09-02 10:45:32 -0700

    added request model

---
+++ dotNET/UserAddRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Requests.Users
+{
+    public class UserAddRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", ErrorMessage = "Password must meet requirements of at least 8 characters, with 1 uppercase, 1 lowercase, 1 number and 1 symbol.")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string PasswordConfirm { get; set; }
+
+		[Required]
+        public string NewRefType { get; set; }
+
+    }
+}
